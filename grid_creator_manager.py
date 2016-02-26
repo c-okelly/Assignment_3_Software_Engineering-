3ab549d036fb503d3eb617fbf792a18c889c3753
c-okelly <okellyconor@gmail.com>
2016-02-26 19:40:56 +0000

    Created empty / fill / toggle seat functions

--- grid_creator_manager.py
+++ grid_creator_manager.py
@@ -8,25 +8,44 @@ def create_grid(x,y):
 
     return gird
 
+# For locationg the correct set we swap the x and y order to better correlate with the graphicl illustration
+
+# find the correct set and occupy it
 def occupy_seat(x,y):
+    grid[y][x] = 1
+
+# Find correct seat and set to empty
+def empty_seat(x,y):
+
+    grid[y][x] = 0
+
+def toggle_seat(x,y):
+
+    #Find current status of seat
+    seat = grid[y][x]
+
+    # Set sat to opposite
+    if seat == 1:
+        empty_seat(x,y)
+    elif seat == 0:
+        occupy_seat(x,y)
+
+
+def generate_coordiantes():
 
     return
 
-def empty_seat():
-
-    return
-
-def toggle_seat():
-
-    return
-
+# Will take the crated grid and instruction set and apply instruction set to grid
 def intruction_manager(grid, instructioino_set):
 
-    return none
+    return
 
 if __name__ == '__main__':
     grid = create_grid(5,5)
     print(grid)
-    grid[0][1] = 1
+    toggle_seat(0,1)
+    toggle_seat(0,1)
     print("hello")
-    print(grid)
+
+
+    print(grid[::-1])
