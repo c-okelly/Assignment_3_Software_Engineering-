4f0ff1c2392952b432f096e23ff580b980caf5dc
c-okelly <okellyconor@gmail.com>
2016-02-26 18:57:29 +0000

    added comment into main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Author = Conor O'Kelly
+
+from parser import generate_instructions
+
+# Main will run all the functions as required
+
+
+def main():
+
+    #Take input file and return instruction list
+
+    # Generate grid
+
+    # perform operations on grid using instruction list
+
+
+if __name__ == '__main__':
+    main()
