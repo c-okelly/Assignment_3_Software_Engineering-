36ee51aa6a37b46d4d3ca515d2a258b8835e6b58
c-okelly <okellyconor@gmail.com>
2016-02-26 19:09:02 +0000

    Basic outline of all fuctionn in grid

--- grid_creator_manager.py
+++ grid_creator_manager.py
@@ -1,2 +1,32 @@
 # Author = Conor O'Kelly
 
+import numpy
+
+def create_grid(x,y):
+
+    gird = numpy.zeros((x,y))
+
+    return gird
+
+def occupy_seat(x,y):
+
+    return
+
+def empty_seat():
+
+    return
+
+def toggle_seat():
+
+    return
+
+def intruction_manager(grid, instructioino_set):
+
+    return none
+
+if __name__ == '__main__':
+    grid = create_grid(5,5)
+    print(grid)
+    grid[0][1] = 1
+    print("hello")
+    print(grid)
