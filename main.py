678d55cea2373585aeef8394110c4cb688796d7e
c-okelly <okellyconor@gmail.com>
2016-02-27 12:37:21 +0000

    Finished main.py off. Program now run succesfully and produces the correct output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Author = Conor O'Kelly
 
-from parser import generate_instructions
+from phraser import generate_instructions
+from grid_creator_manager import create_grid, intruction_manager, count_filled_seats
 
 # Main will run all the functions as required
 
@@ -8,11 +9,24 @@ from parser import generate_instructions
 def main():
 
     #Take input file and return instruction list
+    instructions = generate_instructions("input_assign3.txt")
+    print(len(instructions))
 
     # Generate grid
+    grid = create_grid(1000,1000)
 
     # perform operations on grid using instruction list
+    print("Starting instructions")
+    intruction_manager(grid,instructions)
+
+    # Count number of filled seats
+    filled_seats = count_filled_seats(grid)
+    print(filled_seats)
+
+    return
 
 
 if __name__ == '__main__':
+
     main()
+
