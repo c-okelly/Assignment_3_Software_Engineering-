f7dcfaa1513acde268ed5ca1814e0de8870860c8
cokelly <okellyconor@gmail.com>
2016-02-29 17:59:30 +0000

    Updated main file for cleaner print out of results

--- main.py
+++ main.py
@@ -11,25 +11,27 @@ def main():
 
     curent_time = time.time()
 
+    print("Starting program")
+
+    print("Taking input file")
     #Take input file and return instruction list
     instructions = generate_instructions("input_assign3.txt")
-    print(len(instructions))
 
     # Generate grid
     grid = create_grid(1000,1000)
 
     # perform operations on grid using instruction list
     print("Starting instructions")
-
     generate_indiviudal_instructions_and_run(grid,instructions)
 
     # Count number of filled seats
+    print("Counting seats")
     filled_seats = count_filled_seats(grid)
-    print(filled_seats)
 
-    # Print out run time
+
+    # Find run time
     finish_time = time.time()
-    print("Run time was ",finish_time-curent_time)
+    print("The total number of filled seats was %i and the run time was %f seconds " % (filled_seats, finish_time-curent_time))
 
     return grid
 
